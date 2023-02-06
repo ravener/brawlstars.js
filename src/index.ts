2e1df2b6797f9fe4d656e5ac471d2c20eeaf7475
Raven Ravener <ravener.anime@gmail.com>
2023-02-06 20:03:22 +0430

    fix: include ClubMemberRole and ClubType typings and generate docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 export { Client, ClientOptions, APIError } from "./Client";
 export { Player, PlayerBrawler, PlayerClub, RankingsPlayer, RankingsPlayerClub, PlayerIcon, PlayerBattlelog, PlayerBattlelogEvent, PowerPlaySeason } from "./Player";
-export { Club, ClubMember, RankingsClub } from "./Club";
+export { Club, ClubMember, RankingsClub, ClubMemberRole, ClubType } from "./Club";
 export { Gadget, Gear, Brawler, StarPower } from "./Brawler";
 export { ScheduledEvent, ScheduledEventLocation, EventMode, EventModifier } from "./Event";
 export { cleanTag } from "./utils";
